fd721e7fd29051e111b45a67e17521a6b9b3c9c2
vav200 <vav200@gmail.com>
2024-02-29 08:53:34 +0200

    stage1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import md5 from "crypto-js/md5";
+import { format } from "date-fns";
 
 function App() {
+  const [base, setBase] = useState();
+
+  const url = "http://api.valantis.store:40000/";
+  const password = "Valantis";
+  const dateNow = Date.now();
+  const timestamp = format(dateNow, "yyyyMMdd");
+  const authString = md5(`${password}_${timestamp}`).toString();
+
+  console.log(timestamp);
+  console.log(authString);
+
+  let request = {
+    action: "get_ids",
+    params: { offset: 10, limit: 3 },
+  };
+
+  function get() {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth": authString,
+      },
+      body: JSON.stringify(request),
+    })
+      .then((dat) => {
+        if (!dat.ok) {
+          throw new Error(`Ошибка HTTP: ${dat.status}`);
+        }
+        return dat.json();
+      })
+      .then((dat) => {
+        console.log(dat);
+        setBase(dat);
+      })
+      .catch((error) => {
+        console.error("Ошибка запроса:", error.message);
+      });
+  }
+
+  useEffect(() => {
+    get();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li className="zagl">
+          <div className="box id">№</div>
+          <div className="box name">Название</div>
+          <div className="box price">Цена</div>
+          <div className="box brend">Бренд</div>
+        </li>
+      </ul>
     </div>
   );
 }
